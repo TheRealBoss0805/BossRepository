dd509d64ba2728cf71e9bf562e1677be3d8d18ff
Luis Fernando Rivera <lrivera8@autonoma.edu.pe>
2022-12-28 10:26:52 -0500

    agregue los archivos para la pagina inicio

---
+++ vista/php/inicio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <link rel="stylesheet" href="vista/css/inicio.css">
+        <title>Inicio</title>
+    </head>
+    <body>
+        <div>
+
+        </div>
+    </body>
+</html>
