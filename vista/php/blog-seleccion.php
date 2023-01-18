8dc5f5376e6befdc36028536e94453026d04a670
Luis Fernando Rivera <lrivera8@autonoma.edu.pe>
2023-01-18 18:31:31 -0500

    se realizó ya casi el 90% del fronted, falta trabajar con los js

--- vista/php/blog-seleccion.php
+++ vista/php/blog-seleccion.php
@@ -1,1 +1,38 @@
 <!--falta trabajar-->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="vista/css/blog-seleccion.css">
+    <title>blog-selección</title>
+</head>
+
+<body>
+    <div class="contenedor-blog-seleccion">
+        <div class="content-blog-seleccion">
+            <div> 
+                <img src="vista/imagenes/extras/img-1.png" alt="">
+            </div>
+        </div>
+        <div class="content-blog-seleccion">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum culpa vero doloribus consequatur</p>
+            <p>Responsable, fecha, categoría</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt voluptate itaque quos, fuga eum adipisci nemo animi praesentium sapiente minus quia a, similique.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ea quis incidunt eveniet, consequuntur quia officia eius expedita nihil ipsam laboriosam autem quam. Deserunt saepe ipsa ea eos id suscipit!
+            </p>
+            <div>
+                <img src="vista/imagenes/blog/titulo-blog-seleccion.png" alt="">
+                <h1>Nuestros Proyectos</h1>
+            </div>
+        </div>
+        <div class="content-blog-seleccion">
+            <div class="div-blog-seleccion">
+                <img src="vista/imagenes/extras/img-1.png" alt="">
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
