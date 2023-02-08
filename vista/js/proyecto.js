29361c2d3ed0534045c97e1b85c999d0f1fa81a0
Luis Fernando Rivera <lrivera8@autonoma.edu.pe>
2023-02-08 11:27:56 -0500

    hoy

--- vista/js/proyecto.js
+++ vista/js/proyecto.js
@@ -2,9 +2,8 @@
 FILTRAR POR TIPO
 =============================================*/
 $(".content-proyecto").on("click", ".btnFiltro", function(){
-    
-	var idFiltro = $(this).attr("idFiltro");
-	var datos = new FormData();
+	let idFiltro = $(this).attr("idFiltro");
+	let datos = new FormData();
 	datos.append("idFiltro", idFiltro);
 
 	$.ajax({
@@ -17,5 +16,34 @@ $(".content-proyecto").on("click", ".btnFiltro", function(){
      	success: function(respuesta){
              $("#items").html(respuesta);
      	}
-	})
+	});
+	$(this).parent().children(".btnFiltro").removeClass("active");
+	$(this).parent().children(".btnTodos").removeClass("active");
+	$(this).addClass("active");
+})
+
+
+/*=============================================
+TRAER "TODOS" POR TIPO O POR SECTOR
+=============================================*/
+$(".content-proyecto").on("click", ".btnTodos", function(){
+    
+	let idCategoria = $(this).attr("idCategoria");
+	let datos = new FormData();
+	datos.append("idCategoria", idCategoria);
+
+	$.ajax({
+		url: "ajax/proyectos.ajax.php",
+		method: "POST",
+      	data: datos,
+      	cache: false,
+     	contentType: false,
+     	processData: false,
+     	success: function(respuesta){
+             $("#items").html(respuesta);
+     	}
+	});
+	$(this).parent().children(".btnFiltro").removeClass("active");
+	$(this).removeClass("active");
+	$(this).addClass("active");
 })
